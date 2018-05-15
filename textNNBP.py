8bdfbb4a99d8713d0523ff62a080894fe1c34ede
Oscar024 <or.carvajal.ortiz@gmail.com>
2018-05-15 11:55:07 -0700

    Comparativa de target

--- textNNBP.py
+++ textNNBP.py
@@ -5,11 +5,10 @@ import pyrenn as prn
 
 
 datosIn = pd.read_csv("pacientesreal.csv", sep=',')
-datosOut = pd.read_csv("pacientesrealtarget.csv", sep=',')
+
 
 
 P = np.array(datosIn)
-Y = np.array(datosOut)
 
 filename = "nnBP.csv"
 net=prn.loadNN(filename)
